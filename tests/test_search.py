848bc7adcb2580ac6bef369c6fb9cd98e3b91628
elkham07 <elkhammamedov@gmail.com>
2026-06-26 01:34:34 +0500

    Checking the checkbox and results and Price up to 20,000,000 filter  verification of the upper price limit in the search results

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,28 +1,11 @@
 from playwright.sync_api import Page
 
 def test_search(page: Page):
-    page.goto('https://astana.hh.kz/')
-    page.wait_for_load_state('domcontentloaded')
-    
-    # 2. БОРЬБА С ОКНАМИ: Если вылезет модалка выбора языка, кликаем "Понятно" или закрываем её
-    # Используем мягкий локатор. Если окна нет, тест пойдет дальше через секунду
-    try:
-        if page.locator('button:has-text("Понятно")').is_visible():
-            page.locator('button:has-text("Понятно")').click()
-    except:
-        pass
+    page.goto('https://krisha.kz/')
+    page.wait_for_timeout(1000)
 
-    
-    search_input = page.locator('[data-qa="search-input"]')
-    search_input.click()
-    search_input.fill('QA Engineering')
-    
-    
-    page.get_by_role('button', name='Найти').click()
-    page.wait_for_load_state('networkidle')
-    
-
-    page.locator('text="Фильтры"').click()
-    page.get_by_role('combobox', name='Астана').click()
-    page.get_by_role('checkbox', name='От 1 года до 3').click()
-    page.get_by_role('textbox', name='Уровень дохода', exact=False).fill('300000')
+    page.get_by_role('combobox').nth(2).select_option('2')
+    page.get_by_role("textbox", name="До").click()
+    page.get_by_role("textbox", name="До").fill("2 000 0000")
+    page.get_by_role("button", name="Найти").click()
+    page.wait_for_timeout(1000)
