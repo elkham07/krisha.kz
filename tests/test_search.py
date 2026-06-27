ab26dde824de46c26a98bd6ed771ea79439fc3ca
elkham07 <elkhammamedov@gmail.com>
2026-06-27 23:54:32 +0500

    3 test

--- tests/test_search.py
+++ tests/test_search.py
@@ -22,4 +22,23 @@ def test_appartment2(page: Page):
     page.goto('https://krisha.kz/', wait_until='domcontentloaded')
     page.get_by_role('link', name="Избранное").click()
     page.wait_for_timeout(1000)
+    page.get_by_role("link").nth(5).click()
+    expect(page.get_by_text('Горячие предложения недвижимости в Казахстане')).to_be_visible
+    page.wait_for_timeout(1000)
+
+
+
+
+def test_new_developments(page: Page, context: BrowserContext):
+    page.goto('https://krisha.kz/', wait_until='domcontentloaded')
+    page.get_by_role('link', name="Новостройки").click()
+    page.get_by_role("button", name="Скрыть подсказку").click()
+    expect(page.get_by_text('Новостройки в Казахстане')).to_be_visible
+
+    with context.expect_page() as new_tab_event:
+        page.get_by_role('link', name="Есть видео Isma").click()
+
+        new_tab = new_tab_event.value
+        new_tab.wait_for_load_state()
+
 
